8e32b42ce3c82a35a09c6bcda73e6cad69bdacee
Camila <alvescamila87@gmail.com>
2024-02-27 16:59:29 -0300

    start em java

---
+++ programa01/src/programa01/Programa01.java
@@ -0,0 +1,17 @@
+package programa01;
+
+/**
+ *
+ * @author Camila
+ */
+public class Programa01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello guys!\n");
+        System.out.println("New line with print LN");    
+    }
+    
+}
---
+++ programa02/src/programa02/Programa02.java
@@ -0,0 +1,24 @@
+package programa02;
+
+/**
+ *
+ * @author Camila
+ */
+public class Programa02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int x;
+       x = 5;
+       
+       System.out.println("O valor do x é: " + x);
+       
+       double preco;
+       preco = 1.99;
+       
+       System.out.println("O preço é: " + preco);
+    }
+    
+}
