5f4ac8aca77775a75a7215adc56c5a5a86aa9775
Camila <alvescamila87@gmail.com>
2024-03-08 19:25:16 -0300

    estrutura de dados: collection, arrays, deque, queue, list, setlist, treeset, hashset

--- colecoes/src/colecoes/Arraylist.java
+++ colecoes/src/colecoes/Arraylist.java
@@ -12,7 +12,20 @@ public class Arraylist {
     
     public static void main(String[] args) {
     
+        // List => ArrayList (mais genérica)
         List<String> listaNomes = new ArrayList<>();
+        
+        listaNomes.add("Camila");
+        System.out.println(listaNomes.contains("Camila"));
+        
+        // ArrayList => ArrayList (mais específica)
+        ArrayList<String> listaNomesArray = new ArrayList<>();
+        
+        listaNomesArray.add("Patricia");
+        listaNomesArray.add("Karoline");
+        System.out.println(listaNomesArray.isEmpty());
+        System.out.println(listaNomesArray.contains("Camila"));
+        
     
     }
    
