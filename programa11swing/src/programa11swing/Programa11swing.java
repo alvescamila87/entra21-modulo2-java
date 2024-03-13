6f92cb18e8a94aba49d7cd4b3c339af83ba36a01
Camila <alvescamila87@gmail.com>
2024-03-13 15:51:52 -0300

    GUI Swing jframe

---
+++ programa11swing/src/programa11swing/Programa11swing.java
@@ -0,0 +1,17 @@
+
+package programa11swing;
+
+/**
+ *
+ * @author Camila
+ */
+public class Programa11swing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        janela.main(args);
+    }
+    
+}
