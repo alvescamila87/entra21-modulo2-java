044fbea88cb322e8ffc2df5d27b1ecafd91e4b53
CAMILA ALVES <camila_alves3@estudante.sesisenai.org.br>
2024-03-26 18:54:57 -0300

    collection, arraylist

--- collection/src/collection/TestArrayListNumeros.java
+++ collection/src/collection/TestArrayListNumeros.java
@@ -1,5 +1,8 @@
 package collection;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Camila
@@ -7,6 +10,18 @@ package collection;
 public class TestArrayListNumeros {
     
     public static void main(String[] args) {
+
+        Scanner entrada = new Scanner (System.in);
+        ArrayList<Integer> listaNumeros = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("Informe o %sº número: ", (i + 1));
+            listaNumeros.add(entrada.nextInt());
+        }
+        
+        System.out.println(listaNumeros);
+        
+        entrada.close();
         
     }
     
